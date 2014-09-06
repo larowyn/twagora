94d3246cfd2a25c4d5a8ce0b97f813022dea47c0
Elindorath <elindorath@gmail.com>
2014-09-06 18:17:33 +0200

    fix ta-dropDown

--- app/scripts/directives/dropDownDrv.js
+++ app/scripts/directives/dropDownDrv.js
@@ -5,35 +5,33 @@ angular.module('twagoraApp')
 		return ({
 			restrict: 'A',
 			link: function (scope, elem, attrs) {
-				var open = false;
 				var height = elem[0].offsetHeight;
-				var box = angular.element(elem[0].children[0]);
-				var button = angular.element(elem[0].children[1]);
+				var box = angular.element(elem.children('.debateDesc')[0]);
+				var button = angular.element(elem.children('.debateDescToggle'));
 
 				function showABitElem() {
 					elem.css({
-						'margin-top': -(height - 35) + 'px'
-					});
-					elem.click(showElem);
-					elem.mouseleave(hideElem);
+						'margin-top': -(height - 35) + 'px',
+						'cursor': 'pointer'
+					}).click(showElem).mouseleave(hideElem);
 					box.css({'box-shadow': '0 2px 6px rgba(0, 0, 0, 0.5)'});
 				}
 
 				function showElem() {
-					elem.css({'margin-top': 0});
-					button.off('mouseenter', showABitElem).click(hideElem);
-					elem.off('mouseleave', hideElem);
-					button.addClass('debateDescToggleHide');
+					elem.css({
+						'margin-top': 0,
+						'cursor': 'default'
+					}).off('mouseleave', hideElem);
+					button.off('mouseenter', showABitElem).click(hideElem).addClass('debateDescToggleHide');
 				}
 
 				function hideElem() {
 					elem.css({
-						'margin-top': -(height - 15) + 'px'
-					});
-					elem.off('click', showElem);
-					button.off('click', hideElem).mouseenter(showABitElem);
+						'margin-top': -(height - 15) + 'px',
+						'cursor': 'default'
+					}).off('click', showElem);
 					box.css({'box-shadow': 'none'});
-					button.removeClass('debateDescToggleHide');
+					button.off('click', hideElem).mouseenter(showABitElem).removeClass('debateDescToggleHide');
 				}
 
 				hideElem();
