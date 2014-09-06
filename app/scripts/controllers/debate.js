4a2a9b1962dec830a188b00d0d72a38da4ea73e5
Paul Motte <defaulto60@gmail.com>
2014-09-06 11:53:26 +0200

    chat functionnal

--- app/scripts/controllers/debate.js
+++ app/scripts/controllers/debate.js
@@ -3,29 +3,18 @@
 angular.module('twagoraApp')
 	.controller('DebateCtrl', function ($rootScope, $scope, $routeParams, $firebase, FBURL) {
 
-		/*var roomSync = $firebase(chatRoom);
-		console.log(roomSync);
-		$scope.roomInfo = roomSync.$asObject();
-
-		console.log($scope.roomInfo);*/
-
-		console.log($firebase(new Firebase("https://twagora.firebaseio.com/debates/test/messages")));
-
-		var chatRoom = new Firebase(FBURL + '/debates/' + $routeParams.roomid);
-		//var msgsSync = $firebase(chatRoom.child('messages'));
-		console.log(FBURL + '/debates/' + $routeParams.debateId + '/messages');
-		var msgsSync = $firebase(new Firebase(FBURL + '/debates/' + $routeParams.debateId));
-		msgsSync.$on('loaded', function (data) { console.log(data);$scope.messages = data.messages;})
+		var msgsSync = $firebase(new Firebase(FBURL + '/debates/' + $routeParams.debateId + '/messages'));
 		console.log(msgsSync);
-		//$scope.messages = msgsSync.messages;
-		//$scope.chatMessages = msgsSync.$asArray();
+		$scope.messages = msgsSync.$asArray();
+		console.log($scope.messages);
 
 		$scope.sendMessage = function() {
 			console.log('sendmessage');
 			if ($scope.newMessage.length == 0) return;
 
-			msgsSync.$add({
-				message: $scope.newMessage,
+			$scope.messages.$add({
+				body: $scope.newMessage,
+				from: "anonymous"
 			});
 
 			$scope.newMessage = '';
